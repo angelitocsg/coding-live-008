d586be1dc3aa3fb7104a6b57055cca3ac621b165
Angelito Casagrande <angelito@angelito.com.br>
2020-06-27 12:15:14 -0300

    Add performance improvements

--- src/ApiMapped/Controllers/ProductsController.cs
+++ src/ApiMapped/Controllers/ProductsController.cs
@@ -33,115 +33,152 @@ namespace ApiMapped.Controllers
         [HttpGet("simple")]
         public IActionResult GetSimple()
         {
-            var repository = new ProductRepository();
-            var modelData = repository.GetByAleatoryFilter();
+            dynamic ModelToEntity;
+            dynamic EntityToViewModel;
+            IEnumerable<ProductViewModel> resultViewModels;
 
-            execWatch = new Stopwatch();
-            execWatch.Start();
-            var entities = modelData.Select(mdl => new Product(
-                id: mdl.Id,
-                productName: mdl.ProductName,
-                category: new Category(mdl.Category.Id, mdl.Category.CategoryName),
-                prices: mdl.Prices,
-                codeBar: mdl.Ean13
-            ));
-            var ModelToEntity = GetResult(modelData.Count());
+            using (var repository = new ProductRepository())
+            {
+                var modelData = repository.GetByAleatoryFilter();
 
-            execWatch = new Stopwatch();
-            execWatch.Start();
-            var resultViewModels = entities.Select(entity => new ProductViewModel(
-                id: entity.Id,
-                productName: entity.ProductName,
-                category: entity.Category.CategoryName,
-                price: entity.Prices.Last().Price,
-                codeBar: entity.CodeBar,
-                upperPrice: entity.UpperPrice.Price,
-                lowerPrice: entity.LowerPrice.Price
-            )).ToList();
-            var EntityToViewModel = GetResult(resultViewModels.Count());
+                execWatch = new Stopwatch();
+                execWatch.Start();
+                var entities = modelData.Select(mdl => new Product(
+                    id: mdl.Id,
+                    productName: mdl.ProductName,
+                    category: new Category(mdl.Category.Id, mdl.Category.CategoryName),
+                    prices: mdl.Prices,
+                    codeBar: mdl.Ean13
+                )).ToList();
+                ModelToEntity = GetResult(modelData.Count());
+
+                execWatch = new Stopwatch();
+                execWatch.Start();
+                resultViewModels = entities.Select(entity => new ProductViewModel(
+                   id: entity.Id,
+                   productName: entity.ProductName,
+                   category: entity.Category.CategoryName,
+                   price: entity.Prices.Last().Price,
+                   codeBar: entity.CodeBar,
+                   upperPrice: entity.UpperPrice.Price,
+                   lowerPrice: entity.LowerPrice.Price
+                )).ToList();
+                EntityToViewModel = GetResult(resultViewModels.Count());
+
+                entities = null;
+            }
 
             return Ok(new
             {
                 From = "Simple",
                 ModelToEntity,
                 EntityToViewModel,
-                Data = resultViewModels,
+                Data = resultViewModels.Take(5),
             });
         }
 
         [HttpGet("implicit-operator")]
         public IActionResult GetImplicitOperator()
         {
-            var repository = new ProductRepository();
-            var modelData = repository.GetByAleatoryFilter();
+            dynamic ModelToEntity;
+            dynamic EntityToViewModel;
+            IEnumerable<ProductViewModel> resultViewModels;
 
-            execWatch = new Stopwatch();
-            execWatch.Start();
-            var entities = modelData.Select(mdl => (Product)mdl);
-            var ModelToEntity = GetResult(entities.Count());
+            using (var repository = new ProductRepository())
+            {
+                var modelData = repository.GetByAleatoryFilter();
 
-            execWatch = new Stopwatch();
-            execWatch.Start();
-            var resultViewModels = entities.Select(entity => (ProductViewModel)entity).ToList();
-            var EntityToViewModel = GetResult(entities.Count());
+                execWatch = new Stopwatch();
+                execWatch.Start();
+                var entities = modelData.Select(mdl => (Product)mdl).ToList();
+                ModelToEntity = GetResult(entities.Count());
+
+                execWatch = new Stopwatch();
+                execWatch.Start();
+                resultViewModels = entities.Select(entity => (ProductViewModel)entity).ToList();
+                EntityToViewModel = GetResult(resultViewModels.Count());
+
+                entities = null;
+            }
 
             return Ok(new
             {
                 From = "Implicit operator",
                 ModelToEntity,
                 EntityToViewModel,
-                Data = resultViewModels,
+                Data = resultViewModels.Take(5),
             });
         }
 
         [HttpGet("automapper-1")]
         public IActionResult GetAutoMapper1()
         {
-            var repository = new ProductRepository();
-            var modelData = repository.GetByAleatoryFilter();
+            dynamic ModelToEntity;
+            dynamic EntityToViewModel;
+            IEnumerable<ProductViewModel> resultViewModels;
 
-            execWatch = new Stopwatch();
-            execWatch.Start();
-            var entities = _mapper.Map<IEnumerable<Product>>(modelData);
-            var ModelToEntity = GetResult(entities.Count());
+            using (var repository = new ProductRepository())
+            {
+                var modelData = repository.GetByAleatoryFilter();
 
-            execWatch = new Stopwatch();
-            execWatch.Start();
-            var resultViewModels = _mapper.Map<IEnumerable<ProductViewModel>>(entities);
-            var EntityToViewModel = GetResult(entities.Count());
+                execWatch = new Stopwatch();
+                execWatch.Start();
+                var entities = _mapper.Map<IEnumerable<Product>>(modelData);
+                ModelToEntity = GetResult(entities.Count());
+
+
+                execWatch = new Stopwatch();
+                execWatch.Start();
+                resultViewModels = _mapper.Map<IEnumerable<ProductViewModel>>(entities);
+                EntityToViewModel = GetResult(resultViewModels.Count());
+
+                entities = null;
+            }
 
             return Ok(new
             {
                 From = "AutoMapper 1",
                 ModelToEntity,
                 EntityToViewModel,
-                Data = resultViewModels,
+                Data = resultViewModels.Take(5),
             });
         }
 
         [HttpGet("automapper-2")]
         public IActionResult GetAutoMapper2()
         {
-            var repository = new ProductRepository();
-            var modelData = repository.GetByAleatoryFilterToMapper();
+            dynamic ModelToEntity;
+            dynamic EntityToViewModel;
+            IEnumerable<ProductViewModel> resultViewModels;
 
-            execWatch = new Stopwatch();
-            execWatch.Start();
-            var entities = modelData.ProjectTo<Product>(_mapper.ConfigurationProvider).ToList();
-            var ModelToEntity = GetResult(entities.Count());
-
-            execWatch = new Stopwatch();
-            execWatch.Start();
-            var resultViewModels = _mapper.Map<IEnumerable<ProductViewModel>>(entities);
-            var EntityToViewModel = GetResult(entities.Count());
-
-            return Ok(new
+            try
             {
-                From = "AutoMapper 2",
-                ModelToEntity,
-                EntityToViewModel,
-                Data = resultViewModels,
-            });
+                using (var repository = new ProductRepository())
+                {
+                    var modelData = repository.GetByAleatoryFilterToMapper();
+
+                    execWatch = new Stopwatch();
+                    execWatch.Start();
+                    var entities = modelData.ProjectTo<Product>(_mapper.ConfigurationProvider).ToList();
+                    ModelToEntity = GetResult(entities.Count());
+
+                    execWatch = new Stopwatch();
+                    execWatch.Start();
+                    resultViewModels = _mapper.Map<IEnumerable<ProductViewModel>>(entities);
+                    EntityToViewModel = GetResult(resultViewModels.Count());
+
+                    entities = null;
+                }
+
+                return Ok(new
+                {
+                    From = "AutoMapper 2",
+                    ModelToEntity,
+                    EntityToViewModel,
+                    Data = resultViewModels.Take(5),
+                });
+            }
+            catch (Exception ex) { return BadRequest(ex.Message); }
         }
 
         public object GetResult(int lines)
