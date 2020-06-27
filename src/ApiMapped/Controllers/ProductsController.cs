139479e9ae66bec56dea0e4e7f3487967e332b6f
Angelito Casagrande <angelito@angelito.com.br>
2020-06-27 11:29:26 -0300

    Fix automapper result and add timers

--- src/ApiMapped/Controllers/ProductsController.cs
+++ src/ApiMapped/Controllers/ProductsController.cs
@@ -6,6 +6,7 @@ using ApiMapped.Data.Repositories;
 using ApiMapped.Domain.Entities;
 using ApiMapped.ViewModels;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -33,28 +34,38 @@ namespace ApiMapped.Controllers
         public IActionResult GetSimple()
         {
             var repository = new ProductRepository();
-            var modelData = repository.GetAll();
+            var modelData = repository.GetByAleatoryFilter();
 
+            execWatch = new Stopwatch();
+            execWatch.Start();
             var entities = modelData.Select(mdl => new Product(
                 id: mdl.Id,
                 productName: mdl.ProductName,
-                category: mdl.Category,
+                category: new Category(mdl.Category.Id, mdl.Category.CategoryName),
                 prices: mdl.Prices,
                 codeBar: mdl.Ean13
             ));
+            var ModelToEntity = GetResult(modelData.Count());
 
+            execWatch = new Stopwatch();
+            execWatch.Start();
             var resultViewModels = entities.Select(entity => new ProductViewModel(
                 id: entity.Id,
                 productName: entity.ProductName,
                 category: entity.Category.CategoryName,
                 price: entity.Prices.Last().Price,
-                codeBar: entity.CodeBar
+                codeBar: entity.CodeBar,
+                upperPrice: entity.UpperPrice.Price,
+                lowerPrice: entity.LowerPrice.Price
             )).ToList();
+            var EntityToViewModel = GetResult(resultViewModels.Count());
 
             return Ok(new
             {
-                Result = GetResult(resultViewModels.Count()),
-                // Data = resultViewModels,
+                From = "Simple",
+                ModelToEntity,
+                EntityToViewModel,
+                Data = resultViewModels,
             });
         }
 
@@ -62,16 +73,24 @@ namespace ApiMapped.Controllers
         public IActionResult GetImplicitOperator()
         {
             var repository = new ProductRepository();
-            var modelData = repository.GetAll();
+            var modelData = repository.GetByAleatoryFilter();
 
+            execWatch = new Stopwatch();
+            execWatch.Start();
             var entities = modelData.Select(mdl => (Product)mdl);
+            var ModelToEntity = GetResult(entities.Count());
 
+            execWatch = new Stopwatch();
+            execWatch.Start();
             var resultViewModels = entities.Select(entity => (ProductViewModel)entity).ToList();
+            var EntityToViewModel = GetResult(entities.Count());
 
             return Ok(new
             {
-                Result = GetResult(resultViewModels.Count()),
-                // Data = resultViewModels,
+                From = "Implicit operator",
+                ModelToEntity,
+                EntityToViewModel,
+                Data = resultViewModels,
             });
         }
 
@@ -79,16 +98,49 @@ namespace ApiMapped.Controllers
         public IActionResult GetAutoMapper1()
         {
             var repository = new ProductRepository();
-            var modelData = repository.GetAll();
+            var modelData = repository.GetByAleatoryFilter();
 
+            execWatch = new Stopwatch();
+            execWatch.Start();
             var entities = _mapper.Map<IEnumerable<Product>>(modelData);
+            var ModelToEntity = GetResult(entities.Count());
 
+            execWatch = new Stopwatch();
+            execWatch.Start();
             var resultViewModels = _mapper.Map<IEnumerable<ProductViewModel>>(entities);
+            var EntityToViewModel = GetResult(entities.Count());
 
             return Ok(new
             {
-                Result = GetResult(resultViewModels.Count()),
-                // Data = resultViewModels,
+                From = "AutoMapper 1",
+                ModelToEntity,
+                EntityToViewModel,
+                Data = resultViewModels,
+            });
+        }
+
+        [HttpGet("automapper-2")]
+        public IActionResult GetAutoMapper2()
+        {
+            var repository = new ProductRepository();
+            var modelData = repository.GetByAleatoryFilterToMapper();
+
+            execWatch = new Stopwatch();
+            execWatch.Start();
+            var entities = modelData.ProjectTo<Product>(_mapper.ConfigurationProvider).ToList();
+            var ModelToEntity = GetResult(entities.Count());
+
+            execWatch = new Stopwatch();
+            execWatch.Start();
+            var resultViewModels = _mapper.Map<IEnumerable<ProductViewModel>>(entities);
+            var EntityToViewModel = GetResult(entities.Count());
+
+            return Ok(new
+            {
+                From = "AutoMapper 2",
+                ModelToEntity,
+                EntityToViewModel,
+                Data = resultViewModels,
             });
         }
 
--- src/ApiMapped/ViewModels/ProductViewModel.cs
+++ src/ApiMapped/ViewModels/ProductViewModel.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using ApiMapped.Domain.Entities;
 
@@ -8,19 +9,23 @@ namespace ApiMapped.ViewModels
     {
         private ProductViewModel() { /* for AutoMapper */ }
 
-        public ProductViewModel(Guid id, string productName, string category, decimal price, string codeBar)
+        public ProductViewModel(Guid id, string productName, string category, decimal price, string codeBar, decimal upperPrice, decimal lowerPrice)
         {
             Id = id;
             ProductName = productName;
             Category = category;
             Price = price;
             CodeBar = codeBar;
+            UpperPrice = upperPrice;
+            LowerPrice = lowerPrice;
         }
 
         public Guid Id { get; set; }
         public string ProductName { get; set; }
         public string Category { get; set; }
         public decimal Price { get; set; }
+        public decimal UpperPrice { get; set; }
+        public decimal LowerPrice { get; set; }
         public string CodeBar { get; set; }
 
         public static implicit operator ProductViewModel(Product product)
@@ -30,7 +35,9 @@ namespace ApiMapped.ViewModels
                 productName: product.ProductName,
                 category: product.Category.CategoryName,
                 price: product.Prices.Last().Price,
-                codeBar: product.CodeBar
+                codeBar: product.CodeBar,
+                upperPrice: product.UpperPrice.Price,
+                lowerPrice: product.LowerPrice.Price
             );
         }
 
@@ -40,7 +47,7 @@ namespace ApiMapped.ViewModels
                 id: product.Id,
                 productName: product.ProductName,
                 category: new Category(product.Category),
-                prices: new PriceHistory[] { new PriceHistory(product.Price) },
+                prices: new List<PriceHistory> { new PriceHistory(product.Price) },
                 codeBar: product.CodeBar
             );
         }
