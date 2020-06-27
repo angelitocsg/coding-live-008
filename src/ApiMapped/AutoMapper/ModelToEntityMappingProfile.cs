71bc193e2efefcb18f952c219ec3010020667d90
Angelito Casagrande <angelito@angelito.com.br>
2020-06-27 12:13:41 -0300

    Add mapper condition

--- src/ApiMapped/AutoMapper/ModelToEntityMappingProfile.cs
+++ src/ApiMapped/AutoMapper/ModelToEntityMappingProfile.cs
@@ -7,15 +7,27 @@ namespace ApiMapped.AutoMapper
 {
     public class ModelToEntityMappingProfile : Profile
     {
+        private bool withPrice = true;
+
         public ModelToEntityMappingProfile()
         {
-            CreateMap<ProductModel, Product>()
-                .ForMember(dest => dest.CodeBar, opt => opt.MapFrom(src => src.Ean13))
-                .ForMember(dest => dest.Category, opt => opt.MapFrom(src => src.Category))
-                .ForMember(dest => dest.UpperPrice, opt => opt.MapFrom(src =>
-                    src.Prices.Where(x => x.Price.Equals(src.Prices.Max(y => y.Price))).FirstOrDefault()))
-                .ForMember(dest => dest.LowerPrice, opt => opt.MapFrom(src =>
-                    src.Prices.Where(x => x.Price.Equals(src.Prices.Min(y => y.Price))).FirstOrDefault()));
+
+            if (withPrice)
+            {
+                CreateMap<ProductModel, Product>()
+                    .ForMember(dest => dest.CodeBar, opt => opt.MapFrom(src => src.Ean13))
+                    .ForMember(dest => dest.Category, opt => opt.MapFrom(src => src.Category))
+                    .ForMember(dest => dest.UpperPrice, opt => opt.MapFrom(src =>
+                        src.Prices.Where(x => x.Price.Equals(src.Prices.Max(y => y.Price))).FirstOrDefault()))
+                    .ForMember(dest => dest.LowerPrice, opt => opt.MapFrom(src =>
+                        src.Prices.Where(x => x.Price.Equals(src.Prices.Min(y => y.Price))).FirstOrDefault()));
+            }
+            else
+            {
+                CreateMap<ProductModel, Product>()
+                    .ForMember(dest => dest.CodeBar, opt => opt.MapFrom(src => src.Ean13))
+                    .ForMember(dest => dest.Category, opt => opt.MapFrom(src => src.Category));
+            }
 
             CreateMap<CategoryModel, Category>();
         }
